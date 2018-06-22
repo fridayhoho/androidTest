c57aa446014d7253c25ca64f0b7f2b218e05180a
daguangPro <madiao@foxmail.com>
2018-06-22 17:03:08 +0800

    presenter and View联动 ok

--- app/src/main/java/com/android/samples/arch/componentsbasicsample/ui/homepage/HomeActivity.kt
+++ app/src/main/java/com/android/samples/arch/componentsbasicsample/ui/homepage/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.android.samples.arch.componentsbasicsample.ui.homepage
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import com.android.samples.arch.componentsbasicsample.R
 import com.android.samples.arch.componentsbasicsample.application.TestApplication
 import kotlinx.android.synthetic.main.activity_main.*
@@ -16,16 +17,19 @@ class HomeActivity() : AppCompatActivity(), HomeView {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
         (application as TestApplication).testAppComponent.inject(this)
+        presenter.setView(this)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+            presenter.loadHomePage()
         }
     }
 
     override fun displayHomePage(result: String?) {
-
+        Toast.makeText(this, result, Toast.LENGTH_LONG).show()
     }
 
 }
--- app/src/main/java/com/android/samples/arch/componentsbasicsample/ui/homepage/HomePresenterImpl.kt
+++ app/src/main/java/com/android/samples/arch/componentsbasicsample/ui/homepage/HomePresenterImpl.kt
@@ -1,11 +1,12 @@
 package com.android.samples.arch.componentsbasicsample.ui.homepage
 
 class HomePresenterImpl : HomePresenter {
+    private lateinit var homepageView: HomeView
     override fun setView(homeView: HomeView) {
-
+        this.homepageView = homeView
     }
 
     override fun loadHomePage() {
-
+        this.homepageView.displayHomePage("来自Presenter的result")
     }
 }
