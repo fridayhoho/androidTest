076c5934941fb942bd432dddc9d08195769e08d5
daguangPro <madiao@foxmail.com>
2018-06-26 16:53:21 +0800

    RxAndroid 异步 call ，onDestory时 clear ok

--- app/src/main/java/com/android/samples/arch/componentsbasicsample/ui/detail/DetailActivity.java
+++ app/src/main/java/com/android/samples/arch/componentsbasicsample/ui/detail/DetailActivity.java
@@ -2,19 +2,76 @@ package com.android.samples.arch.componentsbasicsample.ui.detail;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.os.SystemClock;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.android.samples.arch.componentsbasicsample.R;
 
+import java.util.concurrent.Callable;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
 public class DetailActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private static final String TAG = "RxAndroidSamples";
+
+    private final CompositeDisposable disposables = new CompositeDisposable();
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        findViewById(R.id.btnT).setOnClickListener(new View.OnClickListener() {
+            @Override public void onClick(View v) {
+                onRunSchedulerExampleButtonClicked();
+            }
+        });
+    }
+
+    @Override protected void onDestroy() {
+        super.onDestroy();
+        disposables.clear();
+        Log.d(TAG, "onDestory clear");
+    }
+
+    void onRunSchedulerExampleButtonClicked() {
+        disposables.add(sampleObservable()
+                // Run on a background thread
+                .subscribeOn(Schedulers.io())
+                // Be notified on the main thread
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<String>() {
+                    @Override public void onComplete() {
+                        Log.d(TAG, "onComplete()");
+                    }
+
+                    @Override public void onError(Throwable e) {
+                        Log.e(TAG, "onError()", e);
+                    }
+
+                    @Override public void onNext(String string) {
+                        Log.d(TAG, "onNext(" + string + ")");
+                    }
+                }));
+    }
+
+    static Observable<String> sampleObservable() {
+        return Observable.defer(new Callable<ObservableSource<? extends String>>() {
+            @Override public ObservableSource<? extends String> call() throws Exception {
+                // Do some long running operation
+                SystemClock.sleep(1000);
+                return Observable.just("one", "two", "three", "four", "five");
+            }
+        });
     }
 
 
